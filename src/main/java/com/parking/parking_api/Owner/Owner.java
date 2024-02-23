3989807d90ca39c4fa0d5ddf51473062825b9fad
Andres <ricardo.gomez.restrepo@gmail.com>
2024-02-23 15:36:12 -0500

    Architecture pattern implemented, first methods complete, db created

---
+++ src/main/java/com/parking/parking_api/Owner/Owner.java
@@ -0,0 +1,28 @@
+package com.parking.parking_api.Owner;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "owners")
+public class Owner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "own_id")
+    private String ownerId;
+
+    @Basic
+    private String firstName;
+    private String lastName;
+}
